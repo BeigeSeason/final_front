60cd34499af4e0e0633894afda421f90ab7b0dda
sook <jisuk0415@naver.com>
2025-02-20 01:58:48 +0900

    250219 js-내정보수정 하는중

--- src/page/user/MyProfile.tsx
+++ src/page/user/MyProfile.tsx
@@ -5,15 +5,85 @@ import { MyProfileContainer } from "../../style/MypageComponentStyled";
 
 const MyProfile = React.memo(() => {
   const [confirmPw, setConfirmPw] = useState(false);
-  console.log("내 정보 렌더링됨");
+  const [isEditable, setIsEditable] = useState(false);
+  const [selectedMenu, setSelectedMenu] = useState("내 정보 수정");
+  const menuItems = ["내 정보 수정", "비밀번호 변경", "내가 작성한 댓글"];
+  const infoItems = [
+    { label: "이름", value: "박지숙", editable: isEditable },
+    { label: "닉네임", value: "그럴쑥도있지", editable: isEditable },
+    { label: "아이디", value: "jisuk0415", editable: isEditable },
+    { label: "이메일", value: "jisuk0415@naver.com", editable: false },
+  ];
+  const EditInfo = () => {
+    return (
+      <>
+        <div className="info-title">
+          계정 정보
+          <Button $margin="0 20px" onClick={() => setIsEditable(!isEditable)}>
+            {isEditable ? "수정 완료" : "수정하기"}
+          </Button>
+        </div>
+        <div className="info-container">
+          {infoItems.map((item, index) => (
+            <>
+              <div className="info-item">
+                <span className="title content-font1">{item.label}</span>
+                <input
+                  className={`content content-font2 ${
+                    item.editable ? "editable" : ""
+                  }`}
+                  value={item.value}
+                  readOnly={!item.editable}
+                />
+              </div>
+              {index < infoItems.length - 1 && <hr />}
+            </>
+          ))}
+        </div>
+        <div className="info-title">
+          비밀 번호
+          <Button $margin="0 20px" onClick={() => setIsEditable(!isEditable)}>
+            {isEditable ? "변경완료" : "변경하기"}
+          </Button>
+          <Button
+            bgColor="transparent"
+            color="#ccc"
+            border="1px solid #ccc"
+            hoverBgColor="#f1f1f1"
+          >
+            회원 탈퇴
+          </Button>
+        </div>
+      </>
+    );
+  };
+
+  const ChangePw = () => {
+    return (
+      <>
+        <div className="info-item">
+          <span className="title content-font1 pw">신규 비밀번호</span>
+          <input className={`content content-font2 editable pw-input`} />
+        </div>
+        <div className="info-item">
+          <span className="title content-font1 pw">비밀번호 확인</span>
+          <input className={`content content-font2 editable pw-input`} />
+        </div>
+        <div className="button-container pw-button">
+          <Button onClick={() => setIsEditable(!isEditable)}>변경 완료</Button>
+        </div>
+      </>
+    );
+  };
 
   const handleConfirmPw = () => {
     setConfirmPw(true);
   };
+
   return (
     <MyProfileContainer>
       <GlobalFont />
-      {!confirmPw && (
+      {!confirmPw ? (
         <>
           <p className="check-pw content-font1">개인정보 보호를 위해</p>
           <p className="check-pw content-font1">
@@ -32,6 +102,29 @@ const MyProfile = React.memo(() => {
             <p className="validate">비밀번호가 일치하지 않습니다.</p>
             <Button onClick={() => handleConfirmPw()}>확인</Button>
           </div>
+        </>
+      ) : (
+        <>
+          <GlobalFont />
+          <div className="sub-menu">
+            {menuItems.map((menu, index) => (
+              <div key={menu} style={{ display: "inline" }}>
+                <button
+                  className={`content-font1 ${
+                    selectedMenu === menu ? "selected" : ""
+                  }`}
+                  onClick={() => setSelectedMenu(menu)}
+                >
+                  {menu}
+                </button>
+                {index < menuItems.length - 1 && (
+                  <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+                )}
+              </div>
+            ))}
+          </div>
+          {selectedMenu === "내 정보 수정" && <EditInfo />}
+          {selectedMenu === "비밀번호 변경" && <ChangePw />}
         </>
       )}
     </MyProfileContainer>
--- src/style/MypageComponentStyled.tsx
+++ src/style/MypageComponentStyled.tsx
@@ -5,6 +5,7 @@ export const MyProfileContainer = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100%;
+  margin: 0 auto;
 
   .check-pw {
     width: 400px;
@@ -40,5 +41,65 @@ export const MyProfileContainer = styled.div`
       height: 40px;
       border-radius: 10px;
     }
+  }
+
+  .sub-menu {
+    margin: 10px 20px 20px;
+    button {
+      font-size: 14px;
+      background-color: transparent;
+      border: none;
+      cursor: pointer;
+
+      &.selected {
+        font-weight: bold;
+      }
+    }
+  }
+  .info-container {
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    width: 90%;
+    margin: 2px auto 20px;
+  }
+  .info-title {
+    width: 90%;
+    margin: 3px auto 7px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .info-item {
+    display: flex;
+    align-items: center;
+    width: 90%;
+    height: 45px;
+    margin: 2px auto;
+
+    .title {
+      width: 100px;
+      font-size: 16px;
+    }
+    .pw {
+      width: 120px;
+    }
+    .content {
+      border: 1px solid transparent;
+      outline: none;
+      height: 30px;
+      width: 80%;
+      padding: 2px 2px 2px 10px;
+      font-size: 16px;
+    }
+    .content.editable {
+      border: 1px solid black;
+    }
+  }
+  hr {
+    width: 92%;
+    border-top: 1px solid #fff;
+    margin: 1px auto;
+  }
+  .pw-button {
+    width: 76%;
   }
 `;
