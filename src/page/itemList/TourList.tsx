477665ebdb789f58bafa6cc6f73f5bce3f7a87f5
hmya_hnya <kh120205@gmail.com>
2025-02-20 11:06:33 +0900

    250220kh-수정

--- src/page/itemList/TourList.tsx
+++ src/page/itemList/TourList.tsx
@@ -1,5 +1,43 @@
 import { TourItem } from "../../component/ItemComponent";
 
 export const TourList = () => {
-  return <TourItem image="url_to_image" description="관광지 설명" />;
+  const spotsData = [
+    {
+      id: "1",
+      image: "url_to_image_1",
+      description: "관광지 1 설명",
+    },
+    {
+      id: "2",
+      image: "url_to_image_2",
+      description: "관광지 2 설명",
+    },
+    {
+      id: "3",
+      image: "url_to_image_3",
+      description: "관광지 3 설명",
+    },
+    {
+      id: "4",
+      image: "url_to_image_4",
+      description: "관광지 4 설명",
+    },
+    {
+      id: "5",
+      image: "url_to_image_5",
+      description: "관광지 5 설명",
+    },
+  ];
+  return (
+    <div>
+      {spotsData.map((spot) => (
+        <TourItem
+          key={spot.id}
+          image={spot.image}
+          description={spot.description}
+          id={spot.id}
+        />
+      ))}
+    </div>
+  );
 };
