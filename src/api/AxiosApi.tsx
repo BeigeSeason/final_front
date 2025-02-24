32ced385c83dc1539f8bf89eb4ea04fae9ccef18
BeigeSeason <kima606@gmail.com>
2025-02-24 14:30:48 +0900

    2025.02.24 강휘 Axios JWT

--- src/api/AxiosApi.tsx
+++ src/api/AxiosApi.tsx
@@ -49,18 +49,18 @@ const AxiosApi = {
         }
     },
     // 멤버 조회 (전체)
-  memberList: async (page = 1, size = 10, searchType = "NAME", searchValue = "") => {
-    console.log("params:", {page, size, searchType, searchValue});
-    try {
-      const response = await axios.get(`${Common.FINAL_DOMAIN}/member/list`, {
-        params: { page, size, searchType, searchValue },
-      });
-      return response.data;
-    } catch (error) {
-      console.error("멤버 리스트 조회 중 오류 발생:", error);
-      throw error;
-    }
-  },
+    memberList: async (page = 1, size = 10, searchType = "NAME", searchValue = "") => {
+        console.log("params:", {page, size, searchType, searchValue});
+        try {
+            const response = await JwtAxios.get("/member/list", {
+                params: {page, size, searchType, searchValue},
+            });
+            return response.data;
+        } catch (error) {
+            console.error("멤버 리스트 조회 중 오류 발생:", error);
+            throw error;
+        }
+    },
 }
 
 export default AxiosApi;
