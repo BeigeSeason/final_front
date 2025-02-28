fb8b7aa55f7968e3b17f7ef600b1fdb794651e9d
pjs2024 <jisuk0415@naver.com>
2025-02-28 15:13:39 +0900

    250228 js - 다이어리 작성 및 읽어오기

--- src/page/diary/Diary.tsx
+++ src/page/diary/Diary.tsx
@@ -1,15 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { DiaryApi } from "../../api/DiaryApi";
+import parse from "html-react-parser";
+import { DiaryApi, DiaryInfo } from "../../api/DiaryApi";
+import { FaBookmark, FaRegBookmark, FaEllipsisV } from "react-icons/fa";
+import {
+  DiaryContainer,
+  DiaryHeader,
+  DiaryBody,
+} from "../../style/DiaryStyled";
+
+import Profile1 from "../../img/profile/profile1.png";
 
 const Diary = () => {
-  const { diaryId } = useParams();
+  const { diaryId } = useParams<string>();
+  const [diaryInfo, setDiaryInfo] = useState<DiaryInfo | null>();
+  const [isBookmarked, setIsBookmarked] = useState<boolean>(false);
+
+  const timeFormatting = (date: Date | null) => {
+    if (date) {
+      const formattedDate = String(date).slice(2, 10).replaceAll("-", ".");
+      const formattedTime = String(date).slice(11, 16);
+      return `${formattedDate} ${formattedTime}`;
+    }
+  };
 
   useEffect(() => {
     const fetchDiary = async (diaryId?: string) => {
       try {
         if (diaryId) {
           const diary = await DiaryApi.diaryDetail(diaryId);
+          setDiaryInfo(diary);
           console.log("조회된 다이어리:", diary);
         }
       } catch (error) {
@@ -18,10 +38,58 @@ const Diary = () => {
     };
     fetchDiary(diaryId);
   }, []);
+
   return (
-    <>
-      <div>다이어리 페이지입니다.</div>
-    </>
+    <DiaryContainer>
+      <DiaryHeader>
+        <div className="menu-icons">
+          {isBookmarked ? (
+            <>
+              <FaBookmark
+                className="menu-icon"
+                title="북마크"
+                // onClick={() => handleBookmarked()}
+                onClick={() => setIsBookmarked(!isBookmarked)}
+              />
+              <span>숫자</span>
+            </>
+          ) : (
+            <>
+              <FaRegBookmark
+                className="menu-icon"
+                title="북마크"
+                // onClick={() => handleBookmarked()}
+                onClick={() => setIsBookmarked(!isBookmarked)}
+              />
+              <span>숫자</span>
+            </>
+          )}
+          <FaEllipsisV />
+          {/* <button>북마크</button>
+          <button>신고</button>
+          <button>공개/비공개</button>
+          <button>수정</button>
+          <button>삭제</button> */}
+          <h1>{diaryInfo?.title ?? "제목 없음"}</h1>
+          <div className="profile">
+            <div className="profile-img">
+              <img src={Profile1} alt="프로필 이미지" />
+            </div>
+            <div className="profile-info">
+              <p className="nickname">
+                {diaryInfo?.nickname ?? "사용자 닉네임"}
+              </p>
+              <p className="create-time">
+                {timeFormatting(diaryInfo?.createdTime as Date) ?? "작성일"}
+              </p>
+            </div>
+          </div>
+        </div>
+      </DiaryHeader>
+      <DiaryBody>
+        <div>{parse(diaryInfo?.content ?? "내용 없음")}</div>
+      </DiaryBody>
+    </DiaryContainer>
   );
 };
 
